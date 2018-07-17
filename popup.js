8249f73f329c53c4435eae9e0346140cbc6b0d13
Prabhakar Prasad <prabhakarpd7284@gmail.com>
2018-07-17 23:30:16 +0530

    add css, headline search

--- popup.js
+++ popup.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded',function () {
         // alert('dgeyrgfuerhby');
         chrome.tabs.getSelected(null,function (tab) {
             var title = tab.url;
+            var titleu = tab.url;
+            var title1 = $('#headline').val();
+            if(title1!='') title=title1;
             $.ajax({
                 url : 'https://cors.io/?https://click-bait.herokuapp.com/predict?',
                 method : 'GET',
@@ -14,14 +17,16 @@ document.addEventListener('DOMContentLoaded',function () {
                     res = JSON.parse(result);
                     console.log(result);
                     console.log(res['click-bait']);
-                    alert("Url : "+title+"\n Click-Bait : "+res['click-bait']+"\n Non-Click-Bait : "+res['non-click-bait']);
+                    console.log("\nUrl : "+title+"\nEntered Headline : "+title1+"\n Click-Bait : "+res['click-bait']+"\n Non-Click-Bait : "+res['non-click-bait']);
+
+                    $('#result').html("<br>\n<b>Url : </b>"+titleu+"<br>\n<b>Entered Headline : </b>"+title1+"<br>\n <b>Click-Bait : </b>"+res['click-bait']+"<br>\n <b>Non-Click-Bait : </b>"+res['non-click-bait']);
                 },
                 error:function(result){
                     res = JSON.parse(result);
                     console.log(result);
-                    alert("Error in retreiving information.....!!!!!")
-                    // console.log(res.click-bait);
-                    // alert(res.click-bait);
+                    console.log("Error in retreiving information.....!!!!!");
+
+                    $('#result').html("<br>\n<b>Couldn't retreive the information.....</b>");
                 }
             });
         });
